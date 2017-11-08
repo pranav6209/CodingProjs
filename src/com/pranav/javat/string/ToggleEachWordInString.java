ede792c52b9bf84b591a8ed8e54bd90e7cd04540
pranav6209 <pranav.6209@gmail.com>
2017-11-08 12:01:39 -0800

    From WS

---
+++ src/com/pranav/javat/string/ToggleEachWordInString.java
@@ -0,0 +1,21 @@
+package com.pranav.javat.string;
+
+/*
+ * my name is khan
+ * mY nAME iS kHAN
+ * 
+ */
+
+public class ToggleEachWordInString {
+
+	public static String toggleWord(String str) {
+
+		return str;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
