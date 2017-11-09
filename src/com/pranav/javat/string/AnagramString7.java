8bc34feb048d099332c44cd1f04c67e00dc0cdbc
pranav6209 <pranav.6209@gmail.com>
2017-11-08 17:12:58 -0800

    More Practice

---
+++ src/com/pranav/javat/string/AnagramString7.java
@@ -0,0 +1,62 @@
+package com.pranav.javat.string;
+
+import java.util.Arrays;
+
+public class AnagramString7 {
+
+	/*
+	 * Two strings are called anagrams if they contain same set of characters
+	 * but in different order. "keep ? peek", "Mother In Law - Hitler Woman".
+	 */
+
+	private static void checkAnagram(String s1, String s2) {
+
+		// replace all the white spaces
+
+		String str1 = s1.replaceAll("\\s", "");
+		String str2 = s2.replaceAll("\\s", "");
+
+		// if length is not same
+		if (str1.length() != str2.length()) {
+
+			System.out.println("Legnth Not Same.Entered Strings Are Not Anagram");
+
+		}
+
+		else {
+
+			// convert string to lover case and store it to character array
+			char[] array1 = str1.toLowerCase().toCharArray();
+			char[] array2 = str2.toLowerCase().toCharArray();
+
+			// sort the array
+
+			Arrays.sort(array1);
+
+			Arrays.sort(array2);
+
+			// compare array
+
+			if ((Arrays.equals(array1, array2))) {
+
+				System.out.println("Entered Strings Are Anagram");
+
+			}
+
+			else {
+
+				System.out.println("Entered Strings Are Not Anagram");
+			}
+		}
+
+	}
+
+	public static void main(String[] args) {
+
+		checkAnagram("Keep", "peek");
+		checkAnagram("Keep", "peeks");
+		checkAnagram("Mother In Law", "Hitler Woman");
+
+	}
+
+}
---
+++ src/com/pranav/javat/string/PercentageOfUpperCaseLowerCase8.java
@@ -0,0 +1,78 @@
+/**
+ * 
+ */
+package com.pranav.javat.string;
+
+/**
+ * @author pranavpatel
+ * 
+ * Java program to find the percentage of uppercase, lowercase, digits and special characters in a string
+ *
+ */
+public class PercentageOfUpperCaseLowerCase8 {
+
+	private static void checkPercentage(String str) {
+
+		int upperCase = 0;
+		int lowercase = 0;
+		int digits = 0;
+		int other = 0;
+		int strLegnth;
+		
+		// convert string to character array
+		char[] ch = str.toCharArray();
+
+		strLegnth = str.length();
+
+		System.out.println(strLegnth);
+
+		// Iterate through an array and check for uppercase, lowercase, digits and special characters
+		for (int i = 0; i < ch.length; i++) {
+
+			// use Character class to use boolen methods
+			if (Character.isUpperCase(ch[i])) {
+
+				upperCase++;
+
+			}
+
+			else if (Character.isLowerCase(ch[i])) {
+
+				lowercase++;
+
+			}
+
+			else if (Character.isDigit(ch[i])) {
+
+				digits++;
+
+			}
+
+			else {
+
+				other++;
+
+			}
+
+		}
+
+		//convert to percentage.Make sure to parse it (double) else it will keep printing int as both variable are int.
+		
+		// use double. it is always better choice than float.
+		System.out.println("Percentage of Uppercase:-" + (double) (upperCase * 100) / strLegnth+"%");
+
+		System.out.println("Percentage of LowerCase:-" + (double) (lowercase * 100) / strLegnth+"%");
+
+		System.out.println("Percentage of Digits:-" + (double) (digits * 100) / strLegnth+"%");
+
+		System.out.println("Percentage of Othe:-" + (double) (other * 100) / strLegnth+"%");
+
+	}
+
+	public static void main(String[] args) {
+
+		checkPercentage("India is my country 100%");
+
+	}
+
+}
