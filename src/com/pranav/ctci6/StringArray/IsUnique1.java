bfaafdfd17939c6cc02b914e48fe133178a0be5b
pranav6209 <pranav.6209@gmail.com>
2017-11-10 14:38:01 -0800

    More Comment

--- src/com/pranav/ctci6/StringArray/IsUnique1.java
+++ src/com/pranav/ctci6/StringArray/IsUnique1.java
@@ -1,32 +1,55 @@
 package com.pranav.ctci6.StringArray;
 
 public class IsUnique1 {
-	
-	private static boolean isUniqueChars(String str){
-		
-		boolean [] char_set = new boolean[128]; // generates array with initial value false.
-		
-		for (int i = 0; i < str.length(); i++) { // iterate through String
-			
-			//	char ch= str.charAt(i); //
-		
-		int val= str.charAt(i); // returns ascii value.
-			
-			if(char_set[val]){ // if array value true, char is repeating
-				
-				return false;
-				
-			}
-			char_set[val]=true; // make array value true if we come across char
+
+	private static boolean isUniqueChars(String str) {
+
+		/*
+		 * 
+		 * https://stackoverflow.com/questions/19212306/whats-the-difference-
+		 * between-ascii-and-unicode If length is greater than 128, some
+		 * characters must have been repeated. because ASCII is max 128(2^7) if
+		 * it is extended ASCII 256 2^8. Clarify with interviewer
+		 *
+		 */
+
+		if (str.length() > 128) {
+
+			return false;
 		}
-	
+
+		boolean[] char_set = new boolean[128]; // generates array with initial
+												// value false.
+
+		for (int i = 0; i < str.length(); i++) { // iterate through String
+
+			// char ch= str.charAt(i); // returns char at specific index
+
+			int val = str.charAt(i); // returns ascii value.
+
+			/*
+			 * If the value is already true, string has duplicate characters,
+			 * return false
+			 */
+			if (char_set[val]) {
+
+				return false;
+
+			}
+
+			else {
+				char_set[val] = true; // make array value true for char in
+										// string
+			}
+		}
+
 		return true;
 	}
-	
+
 	public static void main(String[] args) {
-		
-		System.out.println(isUniqueChars("My Name"));
-		
+
+		System.out.println(isUniqueChars("My name"));
+
 	}
 
 }
